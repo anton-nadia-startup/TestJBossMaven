89c62ff575bd7c9b114d8ce56d2990a78c7d7c53
Anton Krupnov <oursstartup@gmail.com>
2012-05-24 21:59:28 +0300

    Remove useless code. Rename packages.

---
+++ src/main/java/com/github/anton_nadia_startup/jdbc/requests/Request.java
@@ -0,0 +1,11 @@
+package com.github.anton_nadia_startup.jdbc.requests;
+
+/**
+ * User: Anton Krupnov
+ * Date: 17.04.12
+ * Time: 23:28
+ */
+public interface Request {
+    
+    String getSQLRequest();
+}
